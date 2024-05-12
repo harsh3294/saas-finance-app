1e090689065f1813561acd04b93d716e44279acf
Harsh <57228178+harsh3294@users.noreply.github.com>
2024-05-12 10:53:42 +0530

    chore: initialize Hono.js setup.

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,16 @@
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.get("/hello", (c) => {
+  return c.json({
+    message: "Hello Next.js!",
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
