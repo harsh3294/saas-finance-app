f79bcaec86dd1fa7f95f86a7ad169aa830b816bd
Harsh <57228178+harsh3294@users.noreply.github.com>
2024-06-08 10:59:46 +0530

    feat(loader): added the route loader on top on the page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { QueryProvider } from "@/providers/query-provider";
 import { SheetProvider } from "@/providers/sheet-provider";
 import { Toaster } from "@/components/ui/sonner";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "SaaS Finance Manager",
@@ -21,6 +22,19 @@ export default function RootLayout({
         <body>
           <QueryProvider>
             <SheetProvider />
+            <NextTopLoader
+              initialPosition={0.08}
+              crawlSpeed={200}
+              height={4}
+              crawl={true}
+              showSpinner={true}
+              easing="ease"
+              speed={200}
+              template='<div class="bar" role="bar"><div class="peg"></div></div>
+            <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+              zIndex={1600}
+              showAtBottom={false}
+            />
             <Toaster />
             {children}
           </QueryProvider>
